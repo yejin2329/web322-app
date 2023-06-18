4e4495e75408c7afd36dfc03d187acf3742f1c11
Jenny <jenny222@gmail.com>
2023-06-17 21:04:34 -0400

    update web322-app

--- blog-service.js
+++ blog-service.js
@@ -62,4 +62,61 @@ function getPublishedPosts(){
     });
 }
 
-module.exports={initialize, getAllPosts, getPublishedPosts, getCategories};
+//add addPost
+function addPost(postData){
+    return new Promise((resolve, reject)=>{
+        if(postData.published===undefined){
+            postData.published=false;
+        }else{
+            postData.published=true;
+        }
+
+        postData.id=posts.length+1;
+        posts.push(postData);
+        resolve(postData);
+    });
+}
+
+//add getPostsByCategory(category)
+function getPostsByCategory(category){
+    return new Promise((resolve, reject)=>{
+        const filteredPosts=posts.filter((post)=> String(post.category)===category);
+        
+        if(filteredPosts.length>0){
+            resolve(filteredPosts);
+        }else{
+            reject("No results returned");
+        }
+    });
+}
+
+//add getPostsByMinDate(minDateStr)
+function getPostsByMinDate(minDateStr){
+    return new Promise((resolve, reject)=>{
+        const minDate=new Date(minDateStr);
+        const filteredPosts=posts.filter((post)=>{
+            const postDate=new Date(post.postDate);
+            return postDate>=minDate;
+        });
+        if(filteredPosts.length>0){
+            resolve(filteredPosts);
+        }else{
+            reject("No results returned");
+        }
+    });
+}
+
+//add getPostById(id)
+function getPostById(id){
+    return new Promise((resolve, reject)=>{
+        const post=posts.find((post)=>post.id===id);
+
+        if(post){
+            resolve(post);
+        }else{
+            reject("No result returned");
+        }
+    });
+}
+
+module.exports={initialize, getAllPosts, getPublishedPosts, getCategories, addPost, getPostsByCategory, getPostsByMinDate, getPostById};
--- server.js
+++ server.js
@@ -21,6 +21,22 @@ var app=express();
 
 var path=require('path');
 
+//adding multer, cloudinary and streamifier
+const multer=require("multer");
+const cloudinary=require('cloudinary').v2;
+const streamifier=require('streamifier');
+
+//set cloudinary config 
+cloudinary.config({
+    cloud_name: 'deszzoiq8',
+    api_key: '452493672813397',
+    api_secret: '_m31FeuAQhJQO9nxAZn4KXbX27k',
+    secure: true
+});
+
+//create 'upload' without storage
+const upload=multer(); 
+
 //add static
 app.use(express.static('public'));
 var HTTP_PORT=process.env.PORT||8080;
@@ -62,15 +78,90 @@ app.get("/blog", (req, res)=>{
     });
 });
 
-//add /posts
+//add new route GET/posts/add
+app.get('/posts/add',(req,res)=>{
+    const filePath=path.join(__dirname, 'views', 'addPost.html');
+    res.sendFile(filePath);
+});
+
+//add new route POST
+app.post('/posts/add', upload.single('featureImage'), (req,res)=>{
+    if(req.file){
+        let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+        if (result) {
+        resolve(result);
+        } else {
+        reject(error);
+        }
+        }
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+        };
+        async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+        }
+        upload(req).then((uploaded)=>{
+        processPost(uploaded.url, req.body, res);
+        });
+       }else{
+        processPost("", req.body, res);
+       }
+       function processPost(imageUrl, postData, res){
+        postData.featureImage = imageUrl;
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+        blogService
+        .addPost(postData)
+        .then((newPost)=>{
+            console.log('New post added:', newPost);
+            res.redirect('/posts');
+        })
+        .catch((error)=>{
+            console.error('Error adding post:', error);
+            res.redirect('/posts/add');
+        });
+        
+       }
+    });
+//update /posts
 app.get("/posts", (req, res)=>{
-    blogService.getAllPosts()
+    const category=req.query.category;
+    const minDate=req.query.minDate;
+
+   if(category){
+    blogService
+    .getPostsByCategory(category)
     .then((data)=>{
         res.send(data);
     })
     .catch((error)=>{
         res.status(500).send({message:error});
     });
+   }else if(minDate){
+    blogService
+    .getPostsByMinDate(minDate)
+    .then((data)=>{
+        res.send(data);
+    })
+    .catch((error)=>{
+        res.status(500).send({message:error});
+    });
+   }
+   else{
+    blogService
+    .getAllPosts()
+    .then((data)=>{
+        res.send(data);
+    })
+    .catch((error)=>{
+        res.status(500).send({message:error});
+    });
+   }
 });
 
 //add /categories
@@ -84,7 +175,23 @@ app.get("/categories", (req, res)=>{
     });
 });
 
+//add /post/value
+app.get("/post/:value", (req, res)=>{
+    const postId=parseInt(req.params.value);
+
+    blogService
+    .getPostById(postId)
+    .then((post)=>{
+        res.send(post);
+    })
+    .catch((error)=>{
+        res.status(500).send({message:error});
+    });
+});
+
 //No matching route
 app.use((req, res)=>{
     res.status(404).send("Page Not Found");
 });
+
+
