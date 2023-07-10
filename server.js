5f439429343c8986158c39974d866820bf21623b
Jenny <jenny222@gmail.com>
2023-07-09 23:34:38 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-* WEB322 – Assignment 03
+* WEB322 – Assignment 04
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
 * of this assignment has been copied manually or electronically from any other source 
 * (including 3rd party web sites) or distributed to other students.
 * 
-* Name: Jenny Kim, Student ID: 139787220, Date: June 17, 2023
+* Name: Jenny Kim, Student ID: 139787220, Date: July 9, 2023
 *
 * Cyclic Web App URL: https://tricky-apron-fawn.cyclic.app
 * 
